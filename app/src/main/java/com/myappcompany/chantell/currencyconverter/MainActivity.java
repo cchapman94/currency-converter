c6cae2079f43c1ae0c7f7f35f6d0577dde5c534a
Chantell Chapman <chantellochapman@gmail.com>
2020-08-15 21:32:18 -0700

    Initial commit

---
+++ app/src/main/java/com/myappcompany/chantell/currencyconverter/MainActivity.java
@@ -0,0 +1,43 @@
+package com.myappcompany.chantell.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convertCurrency(View view) {
+        Log.i("Info", "Button pressed");
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+
+        String amountInPounds = editText.getText().toString();
+
+        //double d= Double.parseDouble(yourString);
+        double amountInPoundsDouble= Double.parseDouble(amountInPounds);
+
+        double amountInDollarsDouble = amountInPoundsDouble * 1.3;
+
+        //Convert to 1 decimal place => String numberAsString = Double.toString(number);
+        //String amountInDollarsString = Double.toString(amountInDollarsDouble);
+
+        //Convert to 2 decimal place => String.format("%.2f", d)
+        String amountInDollarsString = String.format("%.2f", amountInDollarsDouble);
+
+
+        Log.i("Amount in dollars", amountInDollarsString);  //Log.i("Info", editText.getText().toString());
+
+
+        Toast.makeText(this, "£" + amountInPounds + " is $" + amountInDollarsString, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
